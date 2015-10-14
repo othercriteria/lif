395b77e9b3307647ae1e80ab5deb5115de5c2618
Daniel Klein <othercriteria@gmail.com>
2015-10-13 23:31:29 -0400

    Adding setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = 'lif',
+      version = '0.1.0',
+      description = 'Game of Life variant with local dynamics',
+      author = 'Daniel Klein',
+      author_email = 'othercriteria@gmail.com',
+      url = 'https://github.com/othercriteria/lif',
+      scripts = ['lif.py'],
+      )
